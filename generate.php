2ce15881e6d7b56929c150d2c2675bc05451b4e3
Alexander Byankin <byankin@sakh.com>
2019-03-11 22:42:03 +1100

    Добавлен генератор по шаблонам

--- generate.php
+++ generate.php
@@ -2,6 +2,7 @@
 require_once __DIR__ . '/common.php';
 
 //$generator = new \TextGen\Generators\LoremIpsumGenerator();
-$generator = new \TextGen\Generators\MarkovGenerator(new \TextGen\Db());
+//$generator = new \TextGen\Generators\MarkovGenerator(new \TextGen\Db());
+$generator = new \TextGen\Generators\PatternGenerator();
 
 echo sprintf('%s написал "%s"', $generator->getName(), $generator->writeArticle()) . PHP_EOL;
