2e3901146f915f0c6cdcb522e17a45d34258e317
hemonugi <hemonugi@sakh.com>
2019-03-16 00:57:26 +1100

    Сгенерированные новости отправляются в случайном порядке

--- telegram.php
+++ telegram.php
@@ -1,6 +1,21 @@
 <?php
+
+use TextGen\Db;
+use TextGen\Generators\MarkovGenerator;
+use TextGen\Generators\NeuralNetworkGenerator;
+use TextGen\Generators\PatternGenerator;
+
 require_once __DIR__ . '/common.php';
 
 $telegram = new \TextGen\Telegram\TelegramSender();
+$rand = rand(1, 100);
 
-$telegram->sendMessage('test');
+if ($rand <= 50) {
+    $generator = new MarkovGenerator(new Db);
+} elseif ($rand >= 50 && $rand <= 75) {
+    $generator = new NeuralNetworkGenerator();
+} elseif ($rand >= 75 && $rand <= 100) {
+    $generator = new PatternGenerator();
+}
+
+$telegram->sendMessage($generator);
